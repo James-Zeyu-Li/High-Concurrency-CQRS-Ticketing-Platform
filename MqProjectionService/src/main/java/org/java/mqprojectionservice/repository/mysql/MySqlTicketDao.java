2e6b0574a3667bf30ee8cf280ff16f11c8b937f0
James_ZY_L <130677389+James-Zeyu-Li@users.noreply.github.com>
2025-11-16 14:36:39 -0800

    Update MQ projection, add DB level de-duplicaiton by unique key,

--- MqProjectionService/src/main/java/org/java/mqprojectionservice/repository/mysql/MySqlTicketDao.java
+++ MqProjectionService/src/main/java/org/java/mqprojectionservice/repository/mysql/MySqlTicketDao.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.java.mqprojectionservice.model.TicketInfo;
 import org.java.mqprojectionservice.model.TicketStatus;
 import org.java.mqprojectionservice.repository.MySqlTicketDAOInterface;
-import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -14,34 +14,41 @@ import java.sql.Timestamp;
 @Repository
 public class MySqlTicketDao implements MySqlTicketDAOInterface {
 
-	private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-	public MySqlTicketDao(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+    public MySqlTicketDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-	@Override
-	public void createTicket(TicketInfo ticketInfo) {
-		String sql = """
-				  INSERT INTO ticket(
-				    ticket_id, venue_id, event_id,
-				    zone_id, row_label, col_label, status,
-				    created_on
-				  ) VALUES(?,?,?,?,?,?,?,?)
-				  ON DUPLICATE KEY UPDATE
-				    status = VALUES(status)
-				""";
-		try {
-			jdbcTemplate.update(
-					sql,
-					ticketInfo.getTicketId(), ticketInfo.getVenueId(), ticketInfo.getEventId(),
-					ticketInfo.getZoneId(), ticketInfo.getRow(), ticketInfo.getColumn(),
-					(ticketInfo.getStatus() == null ? TicketStatus.PENDING_PAYMENT : ticketInfo.getStatus()).name(),
-					Timestamp.from(ticketInfo.getCreatedOn())
-			);
-			log.debug("[MySqlTicketDao] Successfully persisted ticket with id={}", ticketInfo.getTicketId());
-		} catch (DuplicateKeyException e) {
-			log.warn("[MySqlTicketDao] ticketId = {}, exists skip", ticketInfo.getTicketId());
-		}
-	}
+    @Override
+    public void createTicket(TicketInfo ticketInfo) {
+        String sql = """
+                  INSERT INTO ticket(
+                    ticket_id, venue_id, event_id,
+                    zone_id, row_label, col_label, status,
+                    created_on
+                  ) VALUES(?,?,?,?,?,?,?,?)
+                """;
+
+        try {
+            int rowsAffected =
+                    jdbcTemplate.update(sql, ticketInfo.getTicketId(),
+                            ticketInfo.getVenueId(), ticketInfo.getEventId(),
+                            ticketInfo.getZoneId(), ticketInfo.getRow(),
+                            ticketInfo.getColumn(),
+                            (ticketInfo.getStatus() == null ?
+                                    TicketStatus.PENDING_PAYMENT :
+                                    ticketInfo.getStatus()).name(),
+                            Timestamp.from(ticketInfo.getCreatedOn()));
+
+                log.debug(
+                        "[MySqlTicketDao] Successfully persisted ticket with id={}",
+                        ticketInfo.getTicketId());
+            } catch (DataAccessException e) {
+                log.warn(
+                        "[MySqlTicketDao] **SKIPPED** duplicate DB insertion: {}",
+                        ticketInfo.getTicketId());
+            }
+        }
+    }
 }
